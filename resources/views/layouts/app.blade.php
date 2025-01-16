6173bf1c9104f017638443576a023b0792c8ddc6
Khalied Nauly Maturino <khalidmaturino@gmail.com>
2025-01-17 00:20:50 +0700

    footer admin

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,42 +4,30 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-
-        <title>{{ config('app.name', 'DPMPTSP Kota Padang') }}</title>
-
-        <!-- Fonts -->
+        <title>Admin DPMPTSP</title>
+        <link rel="icon" href="https://web.dpmptsp.padang.go.id/assets/Logo_Padang.svg">
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
-        <!-- Scripts -->
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css">
         <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-50">
+        <div class="min-h-screen bg-gray-50 flex flex-col">
             @include('layouts.navigation')
             @include('layouts.sidebar')
-            <main class="p-4 md:ml-64 h-auto pt-20">
+            <main class="p-4 md:ml-64 h-auto pt-20 flex-grow">
                 @yield('app')
             </main>
-
-            <!-- Page Heading -->
-            <!-- @isset($header)
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endisset -->
+            <div class="pt-24">
+                @include('layouts.footer-admin')
+            </div>
         </div>
         <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
         <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
-        @stack('scripts')   
+        @stack('scripts')  
         <script>
-            // Initialize Flowbite modals
             document.addEventListener('DOMContentLoaded', function() {
-                // Initialize all modals
                 const modals = document.querySelectorAll('[data-modal-toggle]');
                 modals.forEach(function(modal) {
                     new Modal(modal);
